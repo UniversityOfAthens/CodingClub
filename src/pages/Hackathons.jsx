c02b642452d7302643d8d57a82cbbff0cccd1cc4
Alekossta <alekosstavrinoudakis@gmail.com>
2024-11-21 22:52:54 +0200

    added images to each section

--- src/pages/Hackathons.jsx
+++ src/pages/Hackathons.jsx
@@ -3,7 +3,18 @@ import { Box, Typography } from "@mui/material";
 export default function Hackathons()
 {
     return <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <Typography variant="h1" component={"h1"} textAlign={"center"} marginY={"2rem"}  sx={{ fontSize: '3rem' }}>
+        <Box
+        component="img"
+        src="/h-logo.png"
+        alt="Logo"
+        sx={{
+          height: "20vh",
+          width: "20vh",
+          marginY: "2rem",
+          alignSelf: "center",
+        }}
+        />
+        <Typography variant="h1" component={"h1"} textAlign={"center"} marginY={"1rem"}  sx={{ fontSize: '3rem' }}>
             Hackathons
         </Typography>
         <Typography>
