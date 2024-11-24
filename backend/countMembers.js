e030c0e6bddf0926f219dd6e111ce3f2d2705021
matinanadali <matinad@gmail.com>
2024-11-24 14:34:19 +0200

    hardcode API URL for Discord member count

--- backend/countMembers.js
+++ backend/countMembers.js
@@ -1,8 +1,11 @@
 export default async function countMembers() {
-  const API_URL = import.meta.env.VITE_REACT_APP_DISCORD_API_URL;
+  const API_URL =
+    "https://discord.com/api/v9/invites/n7BZ9c8tyP?with_counts=true&with_expiration=true";
   try {
     const response = await fetch(API_URL);
+    console.log(response);
     const data = await response.json();
+    console.log(data.approximate_member_count);
     return data.approximate_member_count;
   } catch (error) {
     return -1;
